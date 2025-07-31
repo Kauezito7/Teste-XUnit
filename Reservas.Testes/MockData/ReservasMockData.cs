1ea27729e66f6a480e23c4808e3e9ef5ef93c371
Kaue Moura <Kauehago07@gmail.com>
2025-07-31 13:36:20 -0300

    Teste

---
+++ Reservas.Testes/MockData/ReservasMockData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservas.Api.Models;
+
+namespace Reservas.Testes.MockData
+{
+    public class ReservasMockData
+    {
+        public static List<Reserva> GetReservas()
+        {
+            return new List<Reserva>() 
+            {
+                new Reserva { ReservaId = 1, Nome = "Kaue", InicioLocacao = "Sao Paulo", FimLocacao = "Salvador" },
+                new Reserva { ReservaId = 2, Nome = "Bia", InicioLocacao = "Sao Paulo", FimLocacao = "Rio de Janeiro" },
+                new Reserva { ReservaId = 3, Nome = "Caio", InicioLocacao = "Sao Bernado do campo", FimLocacao = "Sao caetano do sul" }
+            };
+        }
+
+    }
+}
